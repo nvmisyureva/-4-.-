41b59f28ac795e71a50e0b8de1889b4b25d9641d
nvmisyureva <168543591+nvmisyureva@users.noreply.github.com>
2024-07-27 17:21:49 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+#Практическая работа по уроку "Организация программ и методы строк."
+#Не надо ничего говорить. Дождь скажет лучше.
+my_string='Не надо ничего говорить. Дождь скажет лучше.'
+input(my_string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
